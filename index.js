26cad1cb41bd6c0506803b07c73ec35ea64ad3da
Alex Chaus <chausme@gmail.com>
2023-01-01 13:34:39 +0400

    Add initial files

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = arr => {
+    let sorted = true;
+    while (sorted) {
+        sorted = false;
+        for (let i = 0; i < arr.length; i += 1) {
+            if (arr[i] < arr[i + 1]) {
+                const temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+                sorted = true;
+            }
+        }
+    }
+    return arr;
+};

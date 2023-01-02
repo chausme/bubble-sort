64a64cd88c90b6aea427770e207439732b602eef
Alex Chaus <chausme@gmail.com>
2023-01-02 13:57:49 +0400

    Reset sorting from reverse to default

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = arr => {
     while (sorted) {
         sorted = false;
         for (let i = 0; i < arr.length; i += 1) {
-            if (arr[i] < arr[i + 1]) {
+            if (arr[i] > arr[i + 1]) {
                 const temp = arr[i + 1];
                 arr[i + 1] = arr[i];
                 arr[i] = temp;
